25c6e9cf516f86ba772b0d0e06bfd3e4ee78e7ad
rwright1992 <ryanwright1992@gmail.com>
2022-06-16 11:53:43 +0000

    Uploading stage 3 todo list

---
+++ application/routes.py
@@ -0,0 +1,52 @@
+from application import app, db
+from application.models import ToDos
+from flask import redirect, url_for, render_template
+
+@app.route('/')
+def index():
+    todo = ToDos.query.all()
+    # empstr = ""
+    # for t in todo:
+    #     empstr += f'{t.id} {t.task}  {t.completed} <br>' 
+    # return empstr
+    return render_template("task.html", todos=todo)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/add/<t>')
+def add(t):
+    newtask = ToDos(task=t)
+    db.session.add(newtask)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    todo = ToDos.query.get(id)
+    todo.completed = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/incomplete/<int:id>')
+def incomplete(id):
+    todo = ToDos.query.get(id)
+    todo.completed = False
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>/<newtask>')
+def update(id, newtask):
+    todo = ToDos.query.get(id)
+    todo.task = newtask
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<deltask>')
+def delete(deltask):
+    todo = ToDos.query.filter_by(task=deltask).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
